6c1a5f8a131b127a4050bc1a6975f3ef6a76041c
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-18 20:57:57 +0700

    Update panel.php

--- htdocs/application/views/panel.php
+++ htdocs/application/views/panel.php
@@ -57,59 +57,6 @@ $this->load->view('template/sidebar');
                 <!-- ============================================================== -->
                 <!-- Row -->
 
-                <!-- Row -->
-                <!-- Row -->
-                <div class="row">
-                    <!-- Column -->
-                    <div class="col-lg-4 col-xlg-3 col-md-5">
-                        <div class="card blog-widget">
-                            <div class="card-body">
-                                <div class="blog-image"><img src="../assets/images/big/img1.jpg" alt="img" class="img-responsive" /></div>
-                                <h3>Business development new rules for 2017</h3>
-                                <label class="label label-rounded label-success">Technology</label>
-                                <p class="m-t-20 m-b-20">
-                                    Lorem ipsum dolor sit amet, this is a consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                                </p>
-                                <div class="d-flex">
-                                    <div class="read"><a href="javascript:void(0)" class="link font-medium">Read More</a></div>
-                                    <div class="ml-auto">
-                                        <a href="javascript:void(0)" class="link m-r-10 " data-toggle="tooltip" title="Like"><i class="mdi mdi-heart-outline"></i></a> <a href="javascript:void(0)" class="link" data-toggle="tooltip" title="Share"><i class="mdi mdi-share-variant"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-8 col-xlg-9 col-md-7">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex flex-wrap">
-                                    <div>
-                                        <h3 class="card-title">Newsletter Campaign</h3>
-                                        <h6 class="card-subtitle">Overview of Newsletter Campaign</h6>
-                                    </div>
-                                    <div class="ml-auto align-self-center">
-                                        <ul class="list-inline m-b-0">
-                                            <li>
-                                                <h6 class="text-muted text-success"><i class="fa fa-circle font-10 m-r-10 "></i>Open Rate</h6> </li>
-                                            <li>
-                                                <h6 class="text-muted text-info"><i class="fa fa-circle font-10 m-r-10"></i>Recurring Payments</h6> </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="campaign ct-charts"></div>
-                                <div class="row text-center">
-                                    <div class="col-lg-4 col-md-4 m-t-20">
-                                        <h1 class="m-b-0 font-light">5098</h1><small>Total Sent</small></div>
-                                    <div class="col-lg-4 col-md-4 m-t-20">
-                                        <h1 class="m-b-0 font-light">4156</h1><small>Mail Open Rate</small></div>
-                                    <div class="col-lg-4 col-md-4 m-t-20">
-                                        <h1 class="m-b-0 font-light">1369</h1><small>Click Rate</small></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Row -->
                 <div class="row">
                     <!-- Column -->
                     <div class="col-lg-4 col-md-4">
