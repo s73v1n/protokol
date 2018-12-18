d33f514214eb91c16f16bc702ad6f32a23818a61
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-18 10:01:17 +0700

    Update config.php

--- htdocs/application/config/config.php
+++ htdocs/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/protokol';
+$config['base_url'] = 'http://13.76.224.94/protokol';
 
 /*
 |--------------------------------------------------------------------------
