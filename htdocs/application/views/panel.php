ca918315018bc6b5f542b992706047f60d761346
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-19 05:34:01 +0700

    Update panel.php

--- htdocs/application/views/panel.php
+++ htdocs/application/views/panel.php
@@ -112,7 +112,7 @@ $this->load->view('template/sidebar');
                     <!-- Column -->
                     <div class="col-lg-4 col-md-4">
                         <div class="card">
-                            <img class="" src="<?php echo site_url('/assets/images/background/weatherbg.jpg')?>" alt="Card image cap">
+                            <img class="" src="<?php echo base_url('/assets/images/background/weatherbg.jpg')?>" alt="Card image cap">
                             <div class="card-img-overlay" style="height:110px;">
                                 <h3 class="card-title text-white m-b-0 dl">New Delhi</h3>
                                 <small class="card-text text-white font-light">Sunday 15 march</small>
