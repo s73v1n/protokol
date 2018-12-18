97883a4abfbb124f6a42af3a39a02ea09c4125bf
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-19 05:28:38 +0700

    Update head.php

--- htdocs/application/views/template/head.php
+++ htdocs/application/views/template/head.php
@@ -25,9 +25,7 @@
     <link href="<?php echo base_url('/assets/horizontal/css/colors/blue.css')?>" id="theme" rel="stylesheet">
 	<!-- Calendar CSS -->
 	<link href="<?php echo base_url('/assets/plugins/calendar/dist/fullcalendar.css')?>" rel="stylesheet" />
-<?php foreach($css_files as $file): ?>
-	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-<?php endforeach; ?>
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
