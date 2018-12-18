b814d299a34e3aa36765e6c536a3188e7e9398a3
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-18 21:45:10 +0700

    bootstrap gc

--- htdocs/application/controllers/Panel.php
+++ htdocs/application/controllers/Panel.php
@@ -25,9 +25,9 @@ class Panel extends CI_Controller {
 	public function agenda()
 	{
 			$crud = new grocery_CRUD();
-			$crud->set_theme('bootstrap');
+			//$crud->set_theme('bootstrap');
 			//$crud->unset_bootstrap();
-			$crud->unset_jquery();
+			//$crud->unset_jquery();
 			$crud->set_table('tbl_giat');
 			$crud->set_relation('disposisi','tbl_diposisi','perangkat');
 			$crud->set_language('indonesian');
