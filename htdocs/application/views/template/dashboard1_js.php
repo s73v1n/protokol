7d60aded5ee474a7c68a4bab9c70b6fd7484c025
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-25 08:18:55 +0700

    Update dashboard1_js.php

--- htdocs/application/views/template/dashboard1_js.php
+++ htdocs/application/views/template/dashboard1_js.php
@@ -9,3 +9,7 @@
     <script src="<?php echo base_url('/assets/plugins/c3-master/c3.min.js')?>"></script>
     <!-- Chart JS -->
     <script src="<?php echo base_url('/assets/horizontal/js/dashboard1.js')?>"></script>
+	<!-- Editable -->
+    <script src="<?php echo base_url('/assets/plugins/jsgrid/db.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('/assets/plugins/jsgrid/jsgrid.min.js')?>"></script>
+    <script src="<?php echo base_url('/assets/horizontal/js/jsgrid-init.js')?>"></script>
