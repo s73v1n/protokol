9ea624f07a919be6cc49b862b153d9dbbff2845d
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-25 20:13:53 +0700

    Update dashboard.php

--- htdocs/application/views/dashboard.php
+++ htdocs/application/views/dashboard.php
@@ -12,18 +12,18 @@
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
     <title>Material Pro Admin Template - The Most Complete & Trusted Bootstrap 4 Admin Template</title>
     <!-- Bootstrap Core CSS -->
-    <link href="<?php echo base_url ('/assets/plugins/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/plugins/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
     <!-- chartist CSS -->
-    <link href="<?php echo base_url ('/assets/plugins/chartist-js/dist/chartist.min.css')?>" rel="stylesheet">
-    <link href="<?php echo base_url ('/assets/plugins/chartist-js/dist/chartist-init.css')?>" rel="stylesheet">
-    <link href="<?php echo base_url ('/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css')?>" rel="stylesheet">
-    <link href="<?php echo base_url ('/assets/plugins/css-chart/css-chart.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/plugins/chartist-js/dist/chartist.min.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/plugins/chartist-js/dist/chartist-init.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/plugins/css-chart/css-chart.css')?>" rel="stylesheet">
     <!-- Vector CSS -->
-    <link href="<?php echo base_url ('/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css')?>" rel="stylesheet" />
+    <link href="<?php echo base_url('/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css')?>" rel="stylesheet" />
     <!-- Custom CSS -->
-    <link href="<?php echo base_url ('/assets/css/style.css')?>" rel="stylesheet">
+    <link href="<?php echo base_url ('/assets/horizontal/css/style.css')?>" rel="stylesheet">
     <!-- You can change the theme colors from here -->
-    <link href="<?php echo base_url ('/assets/css/colors/blue.css')?>" id="theme" rel="stylesheet">
+    <link href="<?php echo base_url ('/assets/horizontal/css/colors/blue.css')?>" id="theme" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -1194,11 +1194,11 @@
     <script src="<?php echo base_url('/assets/plugins/popper/popper.min.js')?>"></script>
     <script src="<?php echo base_url('/assets/plugins/bootstrap/js/bootstrap.min.js')?>"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="<?php echo base_url ('/assets/js/jquery.slimscroll.js')?>"></script>
+    <script src="<?php echo base_url ('/assets/horizontal/js/jquery.slimscroll.js')?>"></script>
     <!--Wave Effects -->
-    <script src="<?php echo base_url ('/assets/js/waves.js')?>"></script>
+    <script src="<?php echo base_url ('/assets/horizontal/js/waves.js')?>"></script>
     <!--Menu sidebar -->
-    <script src="<?php echo base_url ('/assets/js/sidebarmenu.js')?>"></script>
+    <script src="<?php echo base_url ('/assets/horizontal/js/sidebarmenu.js')?>"></script>
     <!--stickey kit -->
     <script src="<?php echo base_url('/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js')?>"></script>
     <script src="<?php echo base_url('/assets/plugins/sparkline/jquery.sparkline.min.js')?>"></script>
@@ -1207,7 +1207,7 @@
     <script src="<?php echo base_url('/assets/plugins/sparkline/jquery.sparkline.min.js')?>"></script>
     <script src="<?php echo base_url('/assets/plugins/sparkline/jquery.sparkline.min.js')?>"></script>
     <!--Custom JavaScript -->
-    <script src="<?php echo base_url('/assets/js/custom.min.js')?>"></script>
+    <script src="<?php echo base_url('/assets/horizontal/js/custom.min.js')?>"></script>
     <!-- ============================================================== -->
     <!-- This page plugins -->
     <!-- ============================================================== -->
@@ -1217,7 +1217,7 @@
     <!-- Vector map JavaScript -->
     <script src="<?php echo base_url('/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js')?>"></script>
     <script src="<?php echo base_url('/assets/plugins/vectormap/jquery-jvectormap-us-aea-en.js')?>"></script>
-    <script src="<?php echo base_url('/assets/js/dashboard3.js')?>"></script>
+    <script src="<?php echo base_url('/assets/horizontal/js/dashboard3.js')?>"></script>
     <!-- ============================================================== -->
     <!-- Style switcher -->
     <!-- ============================================================== -->
