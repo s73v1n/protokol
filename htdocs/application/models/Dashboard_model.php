23adbe96e209244bc45812ff24a661d86f7eddfe
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-28 08:56:49 +0700

    Update Dashboard_model.php

--- htdocs/application/models/Dashboard_model.php
+++ htdocs/application/models/Dashboard_model.php
@@ -4,7 +4,7 @@ class Dashboard_model extends CI_Model {
 	public function get_all()
 	{
 		$query = $this->db->get('tbl_giat');
-		return $query->result();
+		return $query->result_array();
 	}
 
 }
