7ae5a6134c24b674f88dee5dba89058d2b3b5eee
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-29 13:59:50 +0700

    Update Dashboard_model.php

--- htdocs/application/models/Dashboard_model.php
+++ htdocs/application/models/Dashboard_model.php
@@ -4,8 +4,9 @@ class Dashboard_model extends CI_Model {
 	public function get_all()
 	{
 		$this->db->select('tbl_giat.id as id, tbl_giat.kegiatan as kegiatan, tbl_giat.mulai as start, tbl_giat.selesai as end, tbl_giat.tempat as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
+		$this->db->from('tbl_giat');
 		$this->db->join('tbl_diposisi', 'tbl_diposisi.id = tbl_giat.disposisi');
-		$this->db->get('tbl_giat');
+		$query = $this->db->get();
 		return $query->result_array();
 	}
 
