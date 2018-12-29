d7ba307ddc37b8a26f7c23b7e3db0c495a5fbb60
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-29 10:33:05 +0700

    Update Welcome.php

--- htdocs/application/controllers/Welcome.php
+++ htdocs/application/controllers/Welcome.php
@@ -2,24 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	public function __construct()
+	{
+		$this->load->model('dashboard_model');
+		$this->load->database();
+		$this->load->helper('url');
+	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->view('blank');
+	}
+		public function all_agenda()
+	{
+		$data = $this->Dashboard_model->get_all();
+		echo '<pre>'; print_r($data); echo '</pre>';
 	}
 }
