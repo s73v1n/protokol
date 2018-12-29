78a5791fe947b4c40b55b9a54b7705ff770c33ed
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-29 14:24:55 +0700

    Update tabulasi.php

--- htdocs/application/views/tabulasi.php
+++ htdocs/application/views/tabulasi.php
@@ -296,7 +296,7 @@ $this->load->view('template/menu');
                 <!-- ============================================================== -->
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
-				<div style='height:20px;'></div>  
+				  
 				<div style="padding: 10px">
 					<?php echo $output; ?>
 				</div>
