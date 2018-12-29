0592308863f791390bcdd09fcd0a5614ded94f34
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-29 14:17:33 +0700

    Update tabulasi.php

--- htdocs/application/views/tabulasi.php
+++ htdocs/application/views/tabulasi.php
@@ -70,9 +70,9 @@ $lusa = strftime( "%A, %d %B %Y", strtotime("+2 day"));
                         <!-- Logo icon --><b>
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
-                            <img src="../assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
+                            <img src="<?php echo base_url('/assets/images/logo-icon.png')?>" alt="homepage" class="dark-logo" />
                             <!-- Light Logo icon -->
-                            <img src="../assets/images/logo-light-icon.png" alt="homepage" class="light-logo" />
+                            <img src="<?php echo base_url('/assets/images/logo-light-icon.png')?>" alt="homepage" class="light-logo" />
                         </b>
                         <!--End Logo icon -->
                         <!-- Logo text --><span>
