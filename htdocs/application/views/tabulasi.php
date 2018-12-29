24fb55d5f80260ad12ceb299f245c51fdc52203a
s73v1n <45926318+s73v1n@users.noreply.github.com>
2018-12-29 14:24:07 +0700

    Update tabulasi.php

--- htdocs/application/views/tabulasi.php
+++ htdocs/application/views/tabulasi.php
@@ -212,7 +212,7 @@ $lusa = strftime( "%A, %d %B %Y", strtotime("+2 day"));
                         <!-- Profile -->
                         <!-- ============================================================== -->
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="../assets/images/users/1.jpg" alt="user" class="profile-pic" /></a>
+                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="<?php echo base_url('/assets/images/users/1.jpg')?>" alt="user" class="profile-pic" /></a>
                             <div class="dropdown-menu dropdown-menu-right scale-up">
                                 <ul class="dropdown-user">
                                     <li>
