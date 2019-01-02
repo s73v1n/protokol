3e33d27d97dc39dcf5d58b1bb4cc77d7f0c2424a
s73v1n <45926318+s73v1n@users.noreply.github.com>
2019-01-02 18:09:38 +0700

    Update Welcome.php

--- htdocs/application/controllers/Welcome.php
+++ htdocs/application/controllers/Welcome.php
@@ -12,7 +12,7 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$data = $this->Dashboard_model->get_all();
+		$data['dashboard'] = $this->Dashboard_model->get_all();
 		$this->load->view('blank',$data);
 	}
 	public function all_agenda()
