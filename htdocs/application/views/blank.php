7ffc7b017775686ef0cab39a15e00ef15de64b3c
s73v1n <45926318+s73v1n@users.noreply.github.com>
2019-01-02 08:05:54 +0700

    Update blank.php

--- htdocs/application/views/blank.php
+++ htdocs/application/views/blank.php
@@ -304,8 +304,8 @@ $this->load->view('template/menu');
 									<div class="message-widget message-scroll">
 										<!--pesan-->
 										<div class="card-body">
-											<h4 class="card-title">Card title</h4>
-											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+											<h4 class="card-title"><?php echo $mulai?></h4>
+											<p class="card-text"><?php echo $kegiatan?></p>
 											<a href="#" class="btn btn-primary">Go somewhere</a>
 										</div>
 										<!--pesan-->
