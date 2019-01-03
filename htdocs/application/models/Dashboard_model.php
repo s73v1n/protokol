f0ce34b87a31d8ee45c0b8117baf20567f9d5fb9
s73v1n <45926318+s73v1n@users.noreply.github.com>
2019-01-03 20:28:36 +0700

    Update Dashboard_model.php

--- htdocs/application/models/Dashboard_model.php
+++ htdocs/application/models/Dashboard_model.php
@@ -9,6 +9,15 @@ class Dashboard_model extends CI_Model {
 		$query = $this->db->get();
 		return $query->result_array();
 	}
+	public function agenda_1()
+	{
+		$this->db->select('tbl_giat.id as id, tbl_giat.kegiatan as kegiatan, tbl_giat.mulai as start, tbl_giat.selesai as end, tbl_giat.tempat as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
+		$this->db->from('tbl_giat');
+		$this->db->join('tbl_diposisi', 'tbl_diposisi.id = tbl_giat.disposisi');
+		$this->db->where('DATE(mulai)=CURDATE()');
+		$query = $this->db->get();
+		return $query->result_array();
+	}
 
 }
 ?>
