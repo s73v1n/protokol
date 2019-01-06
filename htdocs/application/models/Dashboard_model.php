8021bfe8c1e5733ead4d345dc5d30bfdc5306c42
zukri01 <zukri@outlook.com>
2019-01-06 20:43:18 +0700

    Update Dashboard_model.php

--- htdocs/application/models/Dashboard_model.php
+++ htdocs/application/models/Dashboard_model.php
@@ -20,7 +20,7 @@ class Dashboard_model extends CI_Model {
 	}
 	public function agenda_2()
 	{
-		$this->db->select('tbl_giat.id as id, tbl_giat.kegiatan as kegiatan, tbl_giat.start as start, tbl_giat.end as end, tbl_giat.description as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
+		$this->db->select('tbl_giat.id as id, tbl_giat.title as kegiatan, tbl_giat.start as start, tbl_giat.end as end, tbl_giat.description as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
 		$this->db->from('tbl_giat');
 		$this->db->join('tbl_diposisi', 'tbl_diposisi.id = tbl_giat.disposisi');
 		$this->db->where('DATE(start)=CURDATE() + INTERVAL 1 DAY');
@@ -29,7 +29,7 @@ class Dashboard_model extends CI_Model {
 	}
 	public function agenda_3()
 	{
-		$this->db->select('tbl_giat.id as id, tbl_giat.kegiatan as kegiatan, tbl_giat.start as start, tbl_giat.end as end, tbl_giat.description as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
+		$this->db->select('tbl_giat.id as id, tbl_giat.title as kegiatan, tbl_giat.start as start, tbl_giat.end as end, tbl_giat.description as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
 		$this->db->from('tbl_giat');
 		$this->db->join('tbl_diposisi', 'tbl_diposisi.id = tbl_giat.disposisi');
 		$this->db->where('DATE(start)=CURDATE() + INTERVAL 2 DAY');
@@ -38,7 +38,7 @@ class Dashboard_model extends CI_Model {
 	}
 	public function get_id($id)
 	{
-		$this->db->select('tbl_giat.id as id, tbl_giat.kegiatan as kegiatan, tbl_giat.start as start, tbl_giat.end as end, tbl_giat.description as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
+		$this->db->select('tbl_giat.id as id, tbl_giat.title as kegiatan, tbl_giat.start as start, tbl_giat.end as end, tbl_giat.description as tempat, tbl_giat.penyelengara as penyelenggara, tbl_diposisi.perangkat as disposisi, tbl_giat.keterangan as keterangan ');
 		$this->db->from('tbl_giat');
 		$this->db->join('tbl_diposisi', 'tbl_diposisi.id = tbl_giat.disposisi');
 		$this->db->where('id',$id);
