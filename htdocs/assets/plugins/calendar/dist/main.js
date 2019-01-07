49ec5f1b75b35c79ee997886656078eae7c1d486
zukri01 <zukri@outlook.com>
2019-01-07 21:13:34 +0700

    Update main.js

--- htdocs/assets/plugins/calendar/dist/main.js
+++ htdocs/assets/plugins/calendar/dist/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    var currentDate; // Holds the day clicked when adding a new event
+    //var currentDate; // Holds the day clicked when adding a new event
     var currentEvent; // Holds the event object when editing an event
 
    $('#color').colorpicker(); // Colopicker
