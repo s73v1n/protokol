afbc1e4b418685ea82a7cb19dd7940a12a6b0379
zukri01 <zukri@outlook.com>
2019-01-08 15:54:45 +0700

    cal

--- htdocs/application/views/blank.php
+++ htdocs/application/views/blank.php
@@ -181,61 +181,82 @@ $this->load->view('template/menu');
                             <div class="card-body">
                                 <div id="calendar"></div>
                                 <!-- BEGIN MODAL -->
-        <div class="modal fade">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                        <h4 class="modal-title"></h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="error"></div>
-                        <form class="form-horizontal" id="crud-form">
-                        <input type="hidden" id="start">
-                        <input type="hidden" id="end">
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="title">Title</label>
-                                <div class="col-md-4">
-                                    <input id="title" name="title" type="text" class="form-control input-md" />
+                                <div class="modal fade" id="create_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <form class="form-horizontal" method="POST" action="POST" id="form_create">
+                                                <input type="hidden" name="calendar_id" value="0">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                      <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    <h4 class="modal-title" id="myModalLabel">Create calendar event</h4>
+                                                </div>
+                                                <div class="modal-body">
+
+                                                    <div class="form-group">
+                                                         <div class="alert alert-danger" style="display: none;"></div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label class="control-label col-sm-2">Title  <span class="required"> * </span></label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" name="title" class="form-control" placeholder="Title">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <label class="control-label col-sm-2">Description</label>
+                                                        <div class="col-sm-10">
+                                                            <textarea name="description" rows="3" class="form-control"  placeholder="Enter description"></textarea>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <label for="color" class="col-sm-2 control-label">Color</label>
+                                                        <div class="col-sm-10">
+                                                            <select name="color" class="form-control">
+                                                                <option value="">Choose</option>
+                                                                <option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
+                                                                <option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
+                                                                <option style="color:#008000;" value="#008000">&#9724; Green</option>                       
+                                                                <option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
+                                                                <option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
+                                                                <option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
+                                                                <option style="color:#000;" value="#000">&#9724; Black</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <label class="control-label col-sm-2">Start Date</label>
+                                                        <div class="col-sm-10">
+                                                            <div class="input-group input-medium date date-picker" data-date-format="yyyy-mm-dd" data-date-viewmode="years">
+                                                                <input type="text" name="start" class="form-control" readonly>
+                                                                <span class="input-group-addon"><i class="fa fa-calendar font-dark"></i></span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <label class="control-label col-sm-2">End Date</label>
+                                                        <div class="col-sm-10">
+                                                            <div class="input-group input-medium date date-picker" data-date-format="yyyy-mm-dd" data-date-viewmode="years">
+                                                                <input type="text" name="end" class="form-control" readonly>
+                                                                <span class="input-group-addon"><i class="fa fa-calendar font-dark"></i></span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <a href="javascript::void" class="btn default" data-dismiss="modal">Cancel</a>
+                                                    <a class="btn btn-danger delete_calendar" style="display: none;">Delete</a>
+                                                    <button type="submit" class="btn green">Submit</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>                            
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="description">Description</label>
-                                <div class="col-md-4">
-                                    <textarea class="form-control" id="description" name="description"></textarea>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="color">Color</label>
-                                <div class="col-md-4">
-                                    <input id="color" name="color" type="text" class="form-control input-md" readonly="readonly" />
-                                    <span class="help-block">Click to pick a color</span>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-                                <!-- Modal Add Category -->
-									<div id="fullCalModal" class="modal fade">
-										<div class="modal-dialog">
-											<div class="modal-content">
-												<div class="modal-header">
-													<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span> <span class="sr-only">close</span></button>
-													<h4 id="modalTitle" class="modal-title"></h4>
-												</div>
-											<div id="modalBody" class="modal-body"></div>
-											<div class="modal-footer">
-												<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-												<button class="btn btn-primary"><a id="eventUrl" target="_blank">Event Page</a></button>
-											</div>
-											</div>
-										</div>
-									</div>
                                 <!-- END MODAL -->
                             </div>
                         </div>
