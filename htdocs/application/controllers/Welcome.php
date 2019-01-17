62363d2fb3c3218cc3d48954e4d26e30fe3b9eea
zukri01 <zukri@outlook.com>
2019-01-17 19:05:13 +0700

    Update Welcome.php

--- htdocs/application/controllers/Welcome.php
+++ htdocs/application/controllers/Welcome.php
@@ -1,6 +1,12 @@
 <?php
+use Restserver\Libraries\REST_Controller;
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+//To Solve File REST_Controller not found
 require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
 class Welcome extends CI_Controller {
 	public function __construct()
 	{
