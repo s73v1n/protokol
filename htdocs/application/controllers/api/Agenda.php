9c10616faffdc71f2aed641b198de5ee63549bed
zukri01 <zukri@outlook.com>
2019-01-17 20:16:33 +0700

    Update Agenda.php

--- htdocs/application/controllers/api/Agenda.php
+++ htdocs/application/controllers/api/Agenda.php
@@ -35,12 +35,8 @@ class Agenda extends REST_Controller {
     }
 	public function agenda_get()
 	{	
-		$agenda = [
-            ['id' => 1, 'name' => 'John', 'email' => 'john@example.com', 'fact' => 'Loves coding'],
-            ['id' => 2, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed on CodeIgniter'],
-            ['id' => 3, 'name' => 'Jane', 'email' => 'jane@example.com', 'fact' => 'Lives in the USA', ['hobbies' => ['guitar', 'cycling']]],
-        ];
-		//$agenda = $this->Dashboard_model->data_all();
+
+		$agenda = $this->Dashboard_model->data_all();
 		$this->response($agenda, REST_Controller::HTTP_OK);
 	}
     public function users_get()
