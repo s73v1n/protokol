a011720fbe41ac49b0cce85336c985cd786637ed
zukri01 <zukri@outlook.com>
2019-01-28 13:22:38 +0700

    Update cal-init.js

--- htdocs/assets/plugins/calendar/dist/cal-init.js
+++ htdocs/assets/plugins/calendar/dist/cal-init.js
@@ -1,6 +1,6 @@
 $(function() {
 	var base_url='http://13.76.224.94/protokol/';
-	$('#date-format').bootstrapMaterialDatePicker({ format : 'YYYY-MM-DD - HH:mm' }); 
+	$('#date-format').bootstrapMaterialDatePicker({ format : 'YYYY-MM-DD HH:mm:ss' }); 
 	$('#calendar').fullCalendar({
 		header: {
             left: 'prev, next, today',
