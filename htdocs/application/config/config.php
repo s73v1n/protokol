234e0d40f9d65e90ae255302504a5ecfb16f593e
zukri01 <zukri@outlook.com>
2019-01-28 19:24:18 +0700

    Update config.php

--- htdocs/application/config/config.php
+++ htdocs/application/config/config.php
@@ -23,16 +23,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-/*
+
 $root = "http://".$_SERVER['HTTP_HOST'];
 $root .= dirname($_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
-
+/*
 $config['base_url'] = "http://".$_SERVER['SERVER_NAME']."/protokol/";
-*/
+
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+*/
 /*
 |--------------------------------------------------------------------------
 | Index File
